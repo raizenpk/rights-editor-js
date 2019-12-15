27a14f9ae4cc93288611fd869094ea14b2e88a56
Essar <glaedrthewise@gmail.com>
2019-12-16 01:17:20 +0200

    Fixed (most likely) memory leak

--- src/app/rights-editor/components/user-permissions/user-permissions.component.ts
+++ src/app/rights-editor/components/user-permissions/user-permissions.component.ts
@@ -1,28 +1,40 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { RoleService } from '../../services/role.service';
 import { Permission } from '../../models/role';
 import { ResourceService } from '../../services/resource.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-user-permissions',
   templateUrl: './user-permissions.component.html',
   styleUrls: [ './user-permissions.component.css' ]
 })
-export class UserPermissionsComponent implements OnChanges {
+export class UserPermissionsComponent implements OnChanges, OnDestroy {
 
   @Input() roles: string[];
 
   public permissions: Permission[] = [];
+
+  private _ngUnsubscribe$ = new Subject();
 
   constructor(private roleService: RoleService, private resourceService: ResourceService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.roles && this.roles && this.roles.length) {
-      this.roleService.getPermissionsForRoles(this.roles).subscribe(permissions => {
-        this.permissions = permissions;
-      });
+      this._ngUnsubscribe$.next();
+      this.roleService.getPermissionsForRoles(this.roles)
+        .pipe(takeUntil(this._ngUnsubscribe$))
+        .subscribe(permissions => {
+          this.permissions = permissions;
+        });
     }
+  }
+
+  ngOnDestroy(): void {
+    this._ngUnsubscribe$.next();
+    this._ngUnsubscribe$.complete();
   }
 
 }
